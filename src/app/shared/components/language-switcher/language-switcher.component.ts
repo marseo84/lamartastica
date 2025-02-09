0b411ed87e2a77a4c97115e5008715046c5ff316
Marcel Bosek <himarseo@gmail.com>
2025-02-09 13:13:53 +0100

    home translation, lang-switcher position

--- src/app/shared/components/language-switcher/language-switcher.component.ts
+++ src/app/shared/components/language-switcher/language-switcher.component.ts
@@ -1,24 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-language-switcher',
-    templateUrl: './language-switcher.component.html',
-    styleUrl: './language-switcher.component.css',
-    standalone: false
+  selector: 'app-language-switcher',
+  templateUrl: './language-switcher.component.html',
+  styleUrl: './language-switcher.component.css',
+  standalone: false,
 })
-export class LanguageSwitcherComponent {
-  selectedLanguage = 'en';
+export class LanguageSwitcherComponent implements OnInit, OnDestroy {
+  selectedLanguage: string = '';
+  private languageChangeSubscription: Subscription | undefined;
 
   constructor(private translate: TranslateService) {
     // this.translate.setDefaultLang('en');
     // this.translate.use('en');
   }
 
+  ngOnInit(): void {
+    this.selectedLanguage =
+      this.translate.currentLang || this.translate.defaultLang;
+
+    this.languageChangeSubscription = this.translate.onLangChange.subscribe(
+      () => {
+        this.selectedLanguage = this.translate.currentLang;
+      }
+    );
+  }
+
   switchLanguage(language: string): void {
     this.selectedLanguage = language;
-    console.log(`Switching language to: ${language}`);
+    // console.log(`Switching language to: ${language}`);
     this.translate.use(language);
-    console.log(`Current language: ${this.translate.currentLang}`);
+    // console.log(`Current language: ${this.translate.currentLang}`);
+  }
+
+  ngOnDestroy(): void {
+    if (this.languageChangeSubscription) {
+      this.languageChangeSubscription.unsubscribe();
+    }
   }
 }
